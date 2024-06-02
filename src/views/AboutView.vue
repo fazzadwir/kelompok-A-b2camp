1777fe84ef4237eadb1a511d90308cfd82df028c
bagja akbar jaya <jundijundy255@gmail.com>
2024-06-02 17:12:01 +0700

    tugas-html-jundi

---
+++ src/views/AboutView.vue
@@ -0,0 +1,197 @@
+
+<template>
+<div class="tablel">
+  <table>
+    <thead>
+      <tr>
+        <th>Id/No</th>
+        <th>Nama</th>
+        <th>Image</th>
+        <th>Color</th>
+        <th>Capacity</th>
+        <th>Chip</th>
+        <th>Deskription</th>
+        <th>Prices</th>
+        <th>Pilih</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--BAGIAN 2-->
+      <tr>
+        <td>02</td>
+        <td>Iphone 11 Pro</td>
+        <td><img src="https://i5.walmartimages.com/asr/57f589d5-ba5e-4c01-ae20-ddc785bb62aa.2ccd44da065375c7015903710607094c.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff" alt=""></td>
+        <td>Gold</td>
+        <td>128 GB</td>
+        <td>Core i3</td>
+        <td>Lorem ipsum dolor sit amet.</td>
+        <td>10.000.000-,</td>
+        <td>
+          <span class="action">
+            <a href="#">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 3-->
+      <tr>
+        <td>03</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>128 GB</td>
+        <td>Core i3</td>
+        <td>Lorem ipsum dolor sit amet.</td>
+        <td>10.000.000-,</td>
+        <td>
+          <span class="action">
+            <a href="#">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 4-->
+      <tr>
+        <td>04</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>128 GB</td>
+        <td>Core i3</td>
+        <td>Lorem ipsum dolor sit amet.</td>
+        <td>10.000.000-,</td>
+        <td>
+          <span class="action">
+            <a href="#">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 5-->
+      <tr>
+        <td>05</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td></td>
+        <td>Core i3</td>
+        <td>Lorem ipsum dolor sit amet.</td>
+        <td>10.000.000-,</td>
+        <td>
+          <span class="action">
+            <a href="#">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 6-->
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 7-->
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 8-->
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 9-->
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+      <!--Bagian 10-->
+      <tr>
+        <td>01</td>
+        <td>Iphone 12 Pro</td>
+        <td><img src="https://spb-apple.ru/image/cache/catalog/apple12/apple%2012%20pro/apple12pro_white_2-700x700.jpg" alt=""></td>
+        <td>cloudy white</td>
+        <td>6 GB/512 GB</td>
+        <td>A14 Bionic</td>
+        <td>iPhone 12 Pro dan iPhone 12 Pro Max didesain dengan fitur canggih</td>
+        <td>6.500.000</td>
+        <td>
+          <span class="action">
+            <a href="../views/PageView.vue">buy</a>
+            <a href="#">keranjang</a>
+          </span>
+        </td>
+      </tr>
+     
+    </tbody>
+  </table>
+</div>
+</template>
+
+<style>
+@import'../assets/scss/tabel.scss'
+</style>
+
---
+++ src/views/PageView.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="awal">
+        <img src="../assets/Group 522.png">
+        <div class="boder"></div>
+   </div>
+   <div class="one">
+    <img src="https://images.pexels.com/photos/1714205/pexels-photo-1714205.jpeg">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Voluptate temporibus voluptatibus blanditiis ratione sed 
+         suscipit ea fuga odit! Laboriosam neque pariatur repellat 
+         corporis assumenda inventore ipsam ea vero quasi. Consequatur,Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Ab ipsa deleniti facilis illo soluta 
+          voluptatum aspernatur aut impedit totam possimus nihil,
+          magni ea veritatis explicabo voluptatem, quod fugit, minus sunt?
+          excepturi itaque! Atque saepe explicabo quasi, obcaecati officii
+          s cupiditate? Eligendi!</p>
+          <div class="p1"></div>
+          </div>
+          <div class="p2">
+            <h3>I Need Help</h3>
+            </div>
+            <div class="p3">
+                <h3><b>what the services</b></h3>
+            </div>
+            <div class="awal1">
+                <h1><b>Lorem Ipsum</b></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aut quos,
+                     quibusdam voluptate repudiandae sed cumque amet officia ad eaque magni velit
+                      aliquam! Tenetur dolores ducimus natus veniam placeat voluptates repellat 
+                      tempora, accusamus perspiciatis a harum temporibus provident impedit molestiae 
+                      quae porro. Perspiciatis itaque, aliquid eius tenetur modi distinctio provident.</p>
+                      <div class="box-1">
+                        <h3>I Need Help</h3>
+                        <div class="bawah">
+                            <h3><b>what the services</b></h3>
+                        </div>
+                      </div>           
+                     </div>
+<div class="awal2">
+    <img src="../assets/Union (1).png">
+</div>
+<div class="box2"></div>
+<div class="isi1">
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Soluta molestiae reprehenderit pariatur ipsum doloremque 
+        nostrum animi temporibus voluptas, nemo saepe? Nesciunt 
+        tempore porro blanditiis illum odio repudiandae nisi optio incidunt.
+    Saepe deserunt cupiditate dolorem quo modi ea molestiae voluptas. Assumenda
+     unde nemo autem id, vel deserunt esse. Autem, illo aliquid, et consequuntur
+      expedita ut sunt, excepturi alias possimus odio est!</p>
+</div>
+
+<div class="bagian3">
+    <h1>We Are Family</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nesciunt nisi quo, iusto alias, odit sed tempore corporis, 
+        accusamus perspiciatis nam expedita possimus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit omnis numquam consequatur quaerat atque voluptate eligendi dicta sunt maiores. Optio neque voluptate temporibus ad laboriosam rerum commodi non placeat eaque.
+        Deleniti porro, fuga et voluptates nesciunt, molestiae culpa ea facilis libero possimus minima provident neque? Quisquam, fuga deleniti quia velit provident, voluptas cum minima expedita minus fugit officia. Numquam, possimus?
+        Alias necessitatibus officiis porro fugit nobis iusto ipsum optio voluptatum sed iste soluta cumque illum nostrum quis id quo commodi, eaque possimus quisquam neque nihil? Dolores impedit fugiat aperiam ratione. aperiam perferendis 
+        laboriosam quod aliquam cupiditate vel dolor.</p>
+        <div class="isi3">
+    <h3>I Need Help</h3>
+</div>
+</div>
+<div class="bagian4"></div>
+<div class="bc">
+        <h1>I Need Help</h1></div>
+      <h1 class="bd">Services</h1>
+
+      <div class="ca">
+        <img src="../assets/Rectangle 250.png">
+      </div>
+      <div class="cc">
+        <img src="../assets/Rectangle 250 (1).png">
+      </div>
+      <div class="cd">
+        <img src="../assets/Rectangle 250 (2).png">
+      </div>
+
+      <div class="da">
+        <h1><b>Section 1</b></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+           Libero ab officiis aut tempore mollitia facilis nulla ex sunt,
+            dicta, minima ullam deserunt saepe neque veritatis debitis ea. Quae, nostrum eos!
+            Nisi ex itaque numquam explicabo, in culpa at omnis dicta quidem
+           perferendis quisquam, vitae ratione ipsa fuga quasi enim odit eaque provident. Earum,
+          in sapiente iure totam velit facilis aut!</p>
+      </div>
+
+      <div class="db">
+        <h1><b>Section 2</b></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Libero ab officiis aut tempore mollitia facilis nulla ex sunt,
+           dicta, minima ullam deserunt saepe neque veritatis debitis ea. Quae, nostrum eos!
+           Nisi ex itaque numquam explicabo, in culpa at omnis dicta quidem
+          perferendis quisquam, vitae ratione ipsa fuga quasi enim odit eaque provident. Earum,
+         in sapiente iure totam velit facilis aut!</p>
+      </div>
+
+      <div class="df">
+        <h1><b>Section 3</b></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Libero ab officiis aut tempore mollitia facilis nulla ex sunt,
+           dicta, minima ullam deserunt saepe neque veritatis debitis ea. Quae, nostrum eos!
+           Nisi ex itaque numquam explicabo, in culpa at omnis dicta quidem
+          perferendis quisquam, vitae ratione ipsa fuga quasi enim odit eaque provident. Earum,
+         in sapiente iure totam velit facilis aut!</p>
+      </div>
+
+      <div class="di">
+        <h1><b>Contact Us</b></h1>
+         <ul>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+          </svg>
+              jundijundy@gmail.com
+            </ul>
+            <ul>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>      
+              </svg>
+              02292235387
+            </ul>
+            <ul>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+              </svg>
+              jl.Cicukang, Arcamanik Bandung
+            </ul>
+        </div>
+        <div class="big">
+            <p>© Copyright Jundillah Salim Al-Ayubi | 2024 |  </p>
+        </div>
+
+</template>
+
+<style lang="scss">
+@import '../assets/scss/main.scss';
+
+</style>
