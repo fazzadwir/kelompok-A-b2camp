c145030a3b92fbfc36dbb523075debdadade3d98
yaksyin maulana <usr@DESKTOP-7HJ06TK>
2024-06-14 03:11:34 +0700

    yaksyin

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import { createBootstrap } from "bootstrap-vue-next";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 import "./assets/scss/style.scss";
+import { convertToRupiah } from 'convert-to-rupiah';
 
 import router from "./router";
 import store from "./store";
 
-const app = createApp(App).use(store).use(router);
+const app = createApp(App).use(store).use(convertToRupiah).use(router);
 app.use(createBootstrap({ components: true, directives: true })); // Change this line
 app.mount("#app");
